9ccbc8e67b48b8377468a8aca455260b31cb9695
bougi82 <32219341+bougi82@users.noreply.github.com>
2018-01-26 08:59:00 +0100

    Add files via upload

---
+++ saveimg.js
@@ -0,0 +1,14 @@
+function capture() {
+    $('#target').html2canvas({
+        onrendered: function (canvas) {
+            //Set hidden field's value to image data (base-64 string)
+            $('#img_val').val(canvas.toDataURL("image/png"));
+            //Submit the form manually
+            document.getElementById("myForm").submit();
+        }
+    });
+}
+
+
+
+
